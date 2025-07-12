81f463170d7dbc980bae4b5def601cd657ba1769
kenil-rajodiya <kenilarajodiya2005@gmail.com>
2025-07-12 23:04:38 +0530

    some changes

--- client/context/AuthContext.jsx
+++ client/context/AuthContext.jsx
@@ -135,7 +135,7 @@ import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 import { io } from "socket.io-client";
 
-const backendUrl = "http://localhost:5000";
+const backendUrl = "https://chat-app-backend-iota-rose.vercel.app/";
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
